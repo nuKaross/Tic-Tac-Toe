16acd5d682d00a68c9f446231035a84dfbe29d22
nuKaross <127345332+nuKaross@users.noreply.github.com>
2023-09-19 22:20:53 +0200

    added win/tie messages

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const createPlayer = (name, symbol) => {
   return { name, symbol }
 }
 
-//Displaycontroller
+//IIFE module
 const Gameboard = (() => {
   let gameBoard = ['', '', '', '', '', '', '', '', '']
 
@@ -22,6 +22,7 @@ const Gameboard = (() => {
       square.addEventListener('click', Game.handleClick)
     })
   }
+  //updates current state of game
   const update = (index, value) => {
     gameBoard[index] = value
     display()
@@ -36,9 +37,21 @@ const Gameboard = (() => {
   }
 })()
 
+//Displays win/lose/tie messages
+const messages = (() => {
+  const winMsg = name => {
+    document.querySelector('#gameMsg').innerHTML = `${name} won`
+  }
+
+  const tieMsg = () => {
+    document.querySelector('#gameMsg').innerHTML = `TIE`
+  }
+  return { winMsg, tieMsg }
+})()
+
 const Game = (() => {
   let players = []
-  let gameOver
+  let gameOver = false
   let currentPlayer
 
   const startGame = () => {
@@ -51,42 +64,61 @@ const Game = (() => {
     Gameboard.display()
   }
 
-  const checkWinner = (board) => {
+  const checkForTie = board => {
+    return board.every(element => element !== '')
+  }
+
+  const checkWinner = board => {
     const winningPatterns = [
       [0, 1, 2],
       [3, 4, 5],
-      [6, 7, 8], 
+      [6, 7, 8],
       [0, 3, 6],
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
       [2, 4, 6],
     ]
-    for(i=0;i<winningPatterns,length,i++) {
-      const [a,b,c] = winningPatterns[i];
-      if(board[a] && board[a] === board[b] &&board[a] === board[c]){
+
+    for (i = 0; i < winningPatterns.length; i++) {
+      const [a, b, c] = winningPatterns[i]
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
         return true
       }
-    }return false
-
+    }
+    return false
   }
   const handleClick = event => {
+    if (gameOver) {
+      return
+    }
     if (Gameboard.getGameboard()[event.target.id] !== '') return
     Gameboard.update(event.target.id, players[currentPlayer].symbol)
     currentPlayer = currentPlayer === 0 ? 1 : 0
 
-    if(checkWinner(Gameboard.getGameboard),players[currentPlayer].symbol){}
+    if (checkWinner(Gameboard.getGameboard(), players[currentPlayer])) {
+      gameOver = true
+      messages.winMsg(players[currentPlayer].name)
+      return
+    }
+    if (checkForTie(Gameboard.getGameboard())) {
+      messages.tieMsg()
+      gameOver = true
+    }
   }
 
   const restartGame = () => {
     for (let i = 0; i < 9; i++) {
       Gameboard.update(i, '')
     }
+
+    Game.startGame()
     Gameboard.display()
   }
 
   return { startGame, handleClick, checkWinner, restartGame }
 })()
+
 const restartBtn = document.querySelector('#restartGame')
 
 restartBtn.addEventListener('click', () => {
